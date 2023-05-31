775d05ed29c4e653a0952ae41ffa97ae7803630c
SifatNiloy <sifatniloy18@gmail.com>
2023-05-31 13:03:57 +0600

    circle animation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React, { useRef, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { TweenMax, Power3 } from 'gsap/src/index';
+import { TweenMax } from 'gsap/gsap-core';
+import { Power3, gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+
+gsap.registerPlugin(ScrollTrigger);
 function App() {
-  let logoItem = useRef(null);
-  let textItem= useRef(null);
+
+  let circle = useRef(null)
+  let circleRed = useRef(null)
+  let circleBlue = useRef(null)
 
   useEffect(() => {
-    console.log(logoItem)
-    TweenMax.to(logoItem,.8,{opacity: 1,y: -20,ease: Power3.easeOut,})
-    TweenMax.to( textItem,.8,{opacity: 1,y: -20, ease: Power3.easeOut,delay: .2 })
-  },[])
+    TweenMax.from(circle, .8, { opacity: 0, x: 40, ease: Power3.ease })
+    TweenMax.from(circleRed, .8, { opacity: 0, x: 40, ease: Power3.ease, delay: .2 })
+    TweenMax.from(circleBlue, .8, { opacity: 0, x: 40, ease: Power3.ease, delay: .4 })
+  }, [])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          ref={el => { logoItem = el }}
-          src={logo} className="App-logo" alt="logo" />
-        <p>
-          ref={el=>{textItem= el}}
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        <div className="circle-container">
+          <div
+            ref={el => circle = el}
+            className="circle"></div>
+          <div
+            ref={el => circleRed = el}
+            className="circle red"></div>
+          <div
+            ref={el => circleBlue = el}
+            className="circle blue"></div>
+        </div>
       </header>
     </div>
   );
