11194c37ea3a76690655a680c5851f528ac20098
SifatNiloy <sifatniloy18@gmail.com>
2023-06-07 12:50:05 +0600

    aos animation effect tried

--- src/Aos/AnimatedPage.js
+++ src/Aos/AnimatedPage.js
@@ -1,9 +1,81 @@
 import React from 'react';
-
+import './AnimatePage.css'
 const AnimatedPage = () => {
     return (
         <div>
             <h1 className='text-7xl'>This is an animated page</h1>
+
+            <div className='card' data-aos="fade-up">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="fade-down">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="fade-right">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="fade-left">
+                <h2>one component</h2>
+            </div>
+            <div className='card' data-aos="fade-up-right">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="fade-up-left">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="fade-down-left">
+                <h2>one component</h2>
+            </div>
+            <div className='card' data-aos="flip-left">
+                <h2>one component</h2>
+            </div>
+            <div className='card' data-aos="flip-right">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="flip-down">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="zoom-in-up">
+                <h2>one component</h2>
+            </div>
+            <div className='card' data-aos="zoom-in-left">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="zoom-in-right">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="zoom-out-right">
+                <h2>one component</h2>
+            </div>
+            <div className='card' data-aos="zoom-out-left">
+                <h2>one component</h2>
+            </div>
+            <div className='card' data-aos="fade-up"
+                data-aos-duration="3000">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="fade-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine">
+                <h2>one component</h2>
+            </div>
+
+            <div className='card' data-aos="flip-left"
+                data-aos-easing="ease-out-cubic"
+                data-aos-duration="2000">
+                <h2>one component</h2>
+            </div>
+
         </div>
     );
 };
--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, { useRef, useEffect } from 'react';
 import './App.css';
-import { TweenMax } from 'gsap/gsap-core';
-import { Power3, gsap } from 'gsap';
+// import { TweenMax } from 'gsap/gsap-core';
+// import { Power3, gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 import AnimatedPage from './Aos/AnimatedPage';
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 function App() {
 
-  let circle = useRef(null)
-  let circleRed = useRef(null)
-  let circleBlue = useRef(null)
+  // let circle = useRef(null)
+  // let circleRed = useRef(null)
+  // let circleBlue = useRef(null)
 
-  useEffect(() => {
-    TweenMax.from(circle, .8, { opacity: 0, x: 40, ease: Power3.ease })
-    TweenMax.from(circleRed, .8, { opacity: 0, x: 40, ease: Power3.ease, delay: .2 })
-    TweenMax.from(circleBlue, .8, { opacity: 0, x: 40, ease: Power3.ease, delay: .4 })
-  }, [])
+  // useEffect(() => {
+  //   TweenMax.from(circle, .8, { opacity: 0, x: 40, ease: Power3.ease })
+  //   TweenMax.from(circleRed, .8, { opacity: 0, x: 40, ease: Power3.ease, delay: .2 })
+  //   TweenMax.from(circleBlue, .8, { opacity: 0, x: 40, ease: Power3.ease, delay: .4 })
+  // }, [])
 
   return (
     <div className="App">
